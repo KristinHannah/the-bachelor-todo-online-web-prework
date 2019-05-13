059f3a73664db1b1bb5d0f3477db08fb9dd34e58
Kristin Kraemer <kristinhannahk@gmail.com>
2019-05-13 21:42:41 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,5 +1,25 @@
+require "pry"
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+  winner_name = ""
+  i = 0 
+  data[season].each do |info|
+   while i < info.length 
+    if data[season][i].include?("winner")
+      info.each do |key, value| 
+        winner_name = data[season][i]["name"]
+    else
+      i += 1 
+    end
+   # info.each do |key, value| 
+    #  binding.pry
+     # if data[season][info]["status"].include?("winner")
+    #    winner_name = "#{data[season][info]["name"]}"
+  # if data[season][status]
+end
+end
+end
+return winner_name
 end
 
 def get_contestant_name(data, occupation)
